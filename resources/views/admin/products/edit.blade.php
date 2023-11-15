b0fc14468bdb46a2cc980ad54bf146b2f5b55cfb
WaiLinnHtetAung <wailinhtetaung007@gmail.com>
2023-11-15 16:12:34 +0630

    update

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -78,7 +78,7 @@
             </div>
             <div class="mt-5">
                 <button class="btn btn-secondary back-btn">Cancel</button>
-                <button class="btn btn-primary">Create</button>
+                <button class="btn btn-primary">Create </button>
             </div>
         </form>
     </div>
